c7b7717f66668a6a1d52af16f45d66afbc4b0974
iuikj <2234514566@qq.com>
2025-05-29 17:28:22 +0800

    完成第一版

---
+++ main.py
@@ -0,0 +1,11 @@
+from dataprocess.academic_warning_gui import AcademicWarningGUI
+
+
+def main():
+    print("=== 学业预警系统 ===")
+    gui = AcademicWarningGUI()
+    gui.run()
+
+
+if __name__ == "__main__":
+    main()
